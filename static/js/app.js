3c619ebfd20a6d3e5aefb73b9bd8b33f45171efc
SethAbbott916 <sethcode91@gmail.com>
2020-07-27 12:57:26 -0700

    Working initial bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,74 @@
 d3.json("samples.json").then(function(data){
     console.log(data);
     var sampledata = Object.values(data.samples);
-    console.log(sampledata);
     var metadata = Object.values(data.metadata);
-    console.log(metadata);
     var namedata = Object.values(data.names);
-    console.log(namedata);
     var svalues = sampledata.map(samples =>  samples.sample_values);
     console.log(svalues)
     var otu_ids = sampledata.map(samples =>  samples.otu_ids);
     console.log(otu_ids)
     var otu_labels = sampledata.map(samples =>  samples.otu_labels);
-    console.log(otu_labels)
     var meta_id = metadata.map(metadata =>  metadata.id);
-    console.log(meta_id)
     var ethnicity = metadata.map(metadata =>  metadata.ethnicity);
-    console.log(ethnicity)
     var gender = metadata.map(metadata =>  metadata.gender);
-    console.log(gender)
     var age = metadata.map(metadata =>  metadata.age);
-    console.log(age)
     var location = metadata.map(metadata =>  metadata.location);
-    console.log(location)
     var bbtype = metadata.map(metadata =>  metadata.bbtype);
-    console.log(bbtype)
     var wfreq = metadata.map(metadata =>  metadata.wfreq);
-    console.log(wfreq)
     initTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq)
     var dropdownMenu = d3.select("#selDataset");
 
     namedata.forEach((name) => {
         var row = dropdownMenu.append("option");
         row.text(name);
-        console.log(name)
     });
 
     function initTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq) {
         var table = d3.select("#sample-metadata");
-        
         table.append("tbody").text("ID: " + meta_id[0]);
-        
         table.append("tbody").text("Ethnicity: " + ethnicity[0]);
-
         table.append("tbody").text("Gender: " + gender[0]);
-
         table.append("tbody").text("Age: " + age[0]);
-
         table.append("tbody").text("Location: " + location[0]);
-
         table.append("tbody").text("bbtype: " + bbtype[0]);
+        table.append("tbody").text("wfreq: " + wfreq[0])
+    };
 
-        table.append("tbody").text("wfreq: " + wfreq[0]);
-        
-
+    var topsvalues = svalues[0].slice(0, 10);
+    var topotuids = otu_ids[0].slice(0, 10);
+    var hoverotu = otu_labels[0].slice(0, 10);
+    console.log(hoverotu)
+    
+    barlabels = []
+    for (var i = 0; i < 10; i++) {
+        barlabels[i] = "OTU " + topotuids[i]; 
     }
+    console.log(barlabels)
+
+    var topsvalues = topsvalues.reverse();
+
+    var barlabels = barlabels.reverse();
+    console.log(barlabels);
+
+    var trace = {
+        x: topsvalues,
+        y: barlabels,
+        text: hoverotu,
+        type: "bar",
+        orientation: "h"
+    };
+
+    var bardata = [trace];
+
+  // Define the plot layout
+    var layout = {
+    xaxis: { title: "Sample Value" },
+    yaxis: { title: "OTU ID"}
+    };
+
+  // Plot the chart to a div tag with id "bar-plot"
+    Plotly.newPlot("bar", bardata, layout);
+
 });
 
 function unpack(rows, index) {
@@ -83,27 +98,28 @@ function optionChanged(value) {
         var wfreq = currentdata[0].wfreq
         buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq);
 
+
         
         function buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq) {
             var table = d3.select("#sample-metadata");
             d3.select("#sample-metadata").html("");
             table.append("tbody").text("ID: " + meta_id);
-            if (ethnicity !== "") {
+            if (ethnicity !== null) {
                 table.append("tbody").text("Ethnicity: " + ethnicity);
             };
-            if (gender !== "") {
+            if (gender !== null) {
                 table.append("tbody").text("Gender: " + gender);
             };
-            if (age !== "") {
+            if (age !== null) {
                 table.append("tbody").text("Age: " + age);
             };
-            if (location !== "") {
+            if (location !== null) {
                 table.append("tbody").text("Location: " + location);
             };
-            if (bbtype !== "") {
+            if (bbtype !== null) {
                 table.append("tbody").text("bbtype: " + bbtype);
             };
-            if (wfreq !== "") {
+            if (wfreq !== null) {
                 table.append("tbody").text("wfreq: " + wfreq);
             };
 
