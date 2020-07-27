005df77952c547a203c634c24f06ee27f734f4c5
SethAbbott916 <sethcode91@gmail.com>
2020-07-26 22:13:10 -0700

    working demographics table

--- static/js/app.js
+++ static/js/app.js
@@ -26,7 +26,7 @@ d3.json("samples.json").then(function(data){
     console.log(bbtype)
     var wfreq = metadata.map(metadata =>  metadata.wfreq);
     console.log(wfreq)
-
+    initTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq)
     var dropdownMenu = d3.select("#selDataset");
 
     namedata.forEach((name) => {
@@ -34,23 +34,86 @@ d3.json("samples.json").then(function(data){
         row.text(name);
         console.log(name)
     });
+
+    function initTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq) {
+        var table = d3.select("#sample-metadata");
+        
+        table.append("tbody").text("ID: " + meta_id[0]);
+        
+        table.append("tbody").text("Ethnicity: " + ethnicity[0]);
+
+        table.append("tbody").text("Gender: " + gender[0]);
+
+        table.append("tbody").text("Age: " + age[0]);
+
+        table.append("tbody").text("Location: " + location[0]);
+
+        table.append("tbody").text("bbtype: " + bbtype[0]);
+
+        table.append("tbody").text("wfreq: " + wfreq[0]);
+        
+
+    }
 });
 
-d3.selectAll("#selDataset").on("change", optionChanged);
-
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+  }    
 
     // This function is called when a dropdown menu item is selected
-    function optionChanged(value) {
+function optionChanged(value) {
             
-        // Use D3 to select the dropdown menu
-        var dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        var dropvalue = dropdownMenu.property("value");
-         console.log(dropvalue)
+    var dropdownMenu = d3.select("#selDataset");
+    var dropvalue = dropdownMenu.property("value");
+    console.log(dropvalue);
 
-        currentdata = sampledata.filter(row => row.id == dropvalue); 
-         console.log(currentdata)
-    };
+    d3.json("samples.json").then(function(data){
+
+        var metadata = Object.values(data.metadata);
+        currentdata = metadata.filter(row => row.id == dropvalue); 
+        console.log(currentdata)
+        var meta_id = currentdata[0].id
+        var ethnicity = currentdata[0].ethnicity
+        var gender = currentdata[0].gender
+        var age = currentdata[0].age
+        var location = currentdata[0].location
+        var bbtype = currentdata[0].bbtype
+        var wfreq = currentdata[0].wfreq
+        buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq);
+
+        
+        function buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq) {
+            var table = d3.select("#sample-metadata");
+            d3.select("#sample-metadata").html("");
+            table.append("tbody").text("ID: " + meta_id);
+            if (ethnicity !== "") {
+                table.append("tbody").text("Ethnicity: " + ethnicity);
+            };
+            if (gender !== "") {
+                table.append("tbody").text("Gender: " + gender);
+            };
+            if (age !== "") {
+                table.append("tbody").text("Age: " + age);
+            };
+            if (location !== "") {
+                table.append("tbody").text("Location: " + location);
+            };
+            if (bbtype !== "") {
+                table.append("tbody").text("bbtype: " + bbtype);
+            };
+            if (wfreq !== "") {
+                table.append("tbody").text("wfreq: " + wfreq);
+            };
+
+        }
+            
+    });
+};
+
+
+
 
 
 
