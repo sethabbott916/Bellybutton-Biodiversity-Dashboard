df9f3bd83ffdf8feea42e28d7c64cb65131688e7
SethAbbott916 <sethcode91@gmail.com>
2020-11-06 17:28:18 -0800

    app.js final edit for comments and efficiency

--- static/js/app.js
+++ static/js/app.js
@@ -55,8 +55,8 @@ d3.json("data/samples.json").then(function(data){
     var topsvalues = topsvalues.reverse();
     var barlabels = barlabels.reverse();
 
-    //specify data being represented in bubble chart
-    var trace2 = {
+    //specify data being represented in bubble chart; format as an object "[]"
+    var bubbledata = [{
         x: otu_ids[0],
         y: svalues[0],
         text: otu_labels[0],
@@ -65,9 +65,7 @@ d3.json("data/samples.json").then(function(data){
           color: otu_ids[0],
           size: svalues[0]
         }
-      };
-      
-    var bubbledata = [trace2];
+      }];
     
     //define layout for bubble chart
     var layout = {
@@ -92,7 +90,7 @@ d3.json("data/samples.json").then(function(data){
   // Plot the chart to a div tag with id "bar-plot"
     Plotly.newPlot("bar", bardata);
 
-    //define plot type and data contained
+    //define gauge plot type and data contained
     var gaugedata = [
         {
           domain: { x: [0, 1], y: [0, 1] },
@@ -123,11 +121,14 @@ d3.json("data/samples.json").then(function(data){
 // This function is called when a dropdown menu item is selected
 function optionChanged(value) {
             
+    //place dropdown value into variable
     var dropdownMenu = d3.select("#selDataset");
     var dropvalue = dropdownMenu.property("value");
 
+    // Use then function to load data into "optionChanged" function
     d3.json("data/samples.json").then(function(data){
-
+        
+        //place the metadata for the subject into individual variables and build table
         var metadata = Object.values(data.metadata);
         currentdata = metadata.filter(row => row.id == dropvalue); 
         var meta_id = currentdata[0].id
@@ -137,8 +138,10 @@ function optionChanged(value) {
         var location = currentdata[0].location
         var bbtype = currentdata[0].bbtype
         var wfreq = currentdata[0].wfreq
+        //buildTable function defined at bottom of code
         buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq);
 
+        //Assign remaining useful data to variables
         var sampledata = Object.values(data.samples);
         sampledata = sampledata.filter(row => row.id == dropvalue);
 
@@ -146,7 +149,8 @@ function optionChanged(value) {
         var otu_ids = sampledata.map(samples =>  samples.otu_ids);
         var otu_labels = sampledata.map(samples =>  samples.otu_labels);
 
-        var trace2 = {
+        //specify data being represented in bubble chart; format as an object "[]"
+        var bubbledata = [{
             x: otu_ids[0],
             y: svalues[0],
             text: otu_labels[0],
@@ -155,24 +159,25 @@ function optionChanged(value) {
               color: otu_ids[0],
               size: svalues[0]
             }
-          };
-          
-        var bubbledata = [trace2];
-          
+          }];
+
+        //define layout for bubble chart
         var layout = {
             title: "OTU ID",
             showlegend: false
         };
           
+        //plot to div: "bubble"
         Plotly.newPlot('bubble', bubbledata, layout);
 
-
+        // assign data for microbes with the highest values to variables
         var topsvalues = svalues[0].slice(0, 10);
         var topotuids = otu_ids[0].slice(0, 10);
         var hoverotu = otu_labels[0].slice(0, 10);
         
         barlabels = []
         
+        //loop to create labels for top microbes
         for (var i = 0; i < 10 ; i++) {
             //fixed bug for subjects with less than 10 results
             if (topotuids[i] !== undefined){
@@ -180,7 +185,7 @@ function optionChanged(value) {
             }
         };
 
-
+        //reverse values to correct bar chart order to descending
         var topsvalues = topsvalues.reverse();
         var barlabels = barlabels.reverse();
 
@@ -196,6 +201,7 @@ function optionChanged(value) {
     // Plot the chart to a div tag with id "bar"
         Plotly.newPlot("bar", bardata);
 
+        //define gauge plot type and data contained
         var gaugedata = [
             {
               domain: { x: [0, 1], y: [0, 1] },
@@ -213,11 +219,12 @@ function optionChanged(value) {
             }
         ];
           
+        //define layout and plot to div: "gauge"
         var gaugelayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
         Plotly.newPlot('gauge', gaugedata, gaugelayout);
 
 
-        
+        //define buildTable function: if variables are not null use them to populate table with metadata
         function buildTable(meta_id, ethnicity, gender, age, location, bbtype, wfreq) {
             var table = d3.select("#sample-metadata");
             d3.select("#sample-metadata").html("");
